975a6b867ba325c2de2dce1e4485aea57154ac14
Bence Ludmány <mokkants@gmail.com>
2018-11-17 21:07:08 +0100

    Setup level generation scripts

---
+++ Assets/Scripts/LevelGeneration/LevelCreator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCreator : MonoBehaviour {
+
+    private JSONParser _jsonParser;
+    private GameObjectCreator _gameObjectCreator;
+
+
+	// Use this for initialization
+	void Start () {
+        _jsonParser = new JSONParser();
+        _gameObjectCreator = new GameObjectCreator();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
