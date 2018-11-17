5b8904f1d8b984452601c2b6d5552b9b3ab2542c
Filip Fatic <gusfatfi@student.gu.se>
2018-11-17 15:37:06 +0100

    initial commit

---
+++ Assets/Scripts/ControlHandler.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Master script for handling user input.
+/// For now, handles user movement and switching cameras.
+/// </summary>
+namespace Project{
+	public class ControlHandler : MonoBehaviour {
+	
+		/// <value>
+		/// Reference to state controller for switching perspective
+		/// </value>
+		public StateController sController;
+
+		/// <value>
+		/// Reference to player gameobject
+		/// </value>
+		public GameObject player;
+
+		/// <value>
+		/// Amount of units by which the player moves per frame.
+		/// Set to public so we can edit it in editor.
+		/// </value>
+		public float MovementFactor = 0.75f;
+
+		Rigidbody playerRb;
+		// Use this for initialization
+		void Start () {
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			CameraListener();
+			MovementListener();
+		}
+
+		/// <summary>
+		/// Listens whether the M key is pressed and switches the two cameras' active state around if it is.
+		/// e.g. If FP.active == true and BE.active == false, it changes the respective values to false and true.
+		/// This switches which camera is active for the user.
+		/// </summary>
+		public void CameraListener(){
+			if (Input.GetKeyDown(KeyCode.M)){
+				switch (sController.GetState()){
+					case StateController.State.FirstPerson:
+					sController.ChangeState(StateController.State.BirdsEye);
+					break;
+					case StateController.State.BirdsEye:
+					sController.ChangeState(StateController.State.FirstPerson);
+					break;
+				}
+			}
+		}
+
+		/// <summary>
+		/// Listens for movement input and moves the player accordingly
+		/// </summary>
+		public void MovementListener(){
+			if (Input.GetKey(KeyCode.W))
+			{
+				player.transform.position += Vector3.forward * MovementFactor ;
+			}
+			else if(Input.GetKey(KeyCode.A))
+			{
+				player.transform.position -= Vector3.right * MovementFactor ;
+			}
+			else if(Input.GetKey(KeyCode.S))
+			{
+				player.transform.position -= Vector3.forward * MovementFactor ;
+			}
+			else if(Input.GetKey(KeyCode.D))
+			{
+				player.transform.position += Vector3.right * MovementFactor ;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/StateController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project{
+	/// <summary>
+	/// Switches between BirdsEye and FirstPerson states
+	/// Delegates behavior relevant to switching states
+	/// </summary>
+	public class StateController : MonoBehaviour {
+
+		/// <value>
+		/// Enumerator denoting different states
+		/// </value>
+
+		/// <value>
+		/// Reference to First Person Camera gameobject
+		/// Found from the player object
+		/// </value>
+		public GameObject FPCamera;
+
+		/// <value>
+		/// Reference to Birds Eye Camera gameobject
+		/// </value>
+		public GameObject BECamera;
+
+		public enum State
+		{
+			BirdsEye,
+			FirstPerson
+		}
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+
+		/// <summary>
+		/// Changes state and initiates relevant behavior
+		/// </summary>
+		/// <param name="state"> State to change to</param>
+		public void ChangeState(State state)
+		{
+			switch(state)
+			{
+				//TODO: Future behavior that will depend on changed states
+				case State.BirdsEye:
+				break;
+
+				case State.FirstPerson:
+				break;
+
+				default:
+				break;
+			}
+			FPCamera.SetActive(!FPCamera.activeSelf);
+			BECamera.SetActive(!BECamera.activeSelf);
+		}
+
+		/// <summary>
+		/// Function to check the camera state of the system.
+		/// </summary>
+		/// <returns>The current state.</returns>
+		public State GetState()
+		{
+			if(FPCamera.activeSelf)
+			{
+				return State.FirstPerson;
+			}
+
+			else if(BECamera.activeSelf)
+			{
+				return State.BirdsEye;
+			}
+			else
+			{
+				Debug.LogWarning("StateController: No cameras active. What the fuck? Defaulting to FirstPerson");
+				return State.FirstPerson;	
+			}
+		}
+	}
+}
+
