dae46069cf40a5cb262470ab327a5a48fa0d1587
Filip Fatic <gusfatfi@student.gu.se>
2018-12-02 15:56:34 +0100

    added files from npcs branch

---
+++ Assets/Scripts/Models/NPCObject.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project
+{
+		public class NPCObject{
+		public enum npcType {
+			Integer,
+			Double,
+			Float,
+			Boolean,
+			Char,
+			Short,
+			Long
+
+		}
+		
+		public npcType type;
+		public string varName;
+		public string value;
+
+		public NPCObject(npcType type, string varName, string value)
+		{
+			this.type = type;
+			this.varName = varName;
+			this.value = value;
+		}
+
+		public override string ToString()
+		{
+			//todo
+			string sType;
+			switch(this.type)
+			{
+				case(npcType.Integer):
+				sType = "Integer";
+				break;
+				case(npcType.Double):
+				sType = "Double";
+				break;
+				case(npcType.Float):
+				sType = "Float";
+				break;
+				case(npcType.Boolean):
+				sType = "Boolean";
+				break;
+				case(npcType.Char):
+				sType = "Char";
+				break;
+				case(npcType.Short):
+				sType = "Short";
+				break;
+				case(npcType.Long):
+				sType = "Long";
+				break;
+				default:
+				sType = "Undefined";
+				break;
+			}
+			string txt = "Name: " + this.varName + "\nType: " + sType + "\nValue: " + this.value + "\n";
+			return txt;
+		}
+
+	}
+}
---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project
+{
+	public class NPC : MonoBehaviour, IInteractable {
+
+		private NPCObject npcInfo;
+		// Use this for initialization
+		void Start () {
+			//DEBUG STATEMENTS
+			NPCObject testInfo = new NPCObject(NPCObject.npcType.Integer, "ExampleName", "2" );
+			Populate(testInfo);
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			
+		}
+		
+		public void Interact()
+		{
+			Debug.Log("Interaction method run");
+			this.gameObject.GetComponent<MeshRenderer>().enabled = true;
+			StartCoroutine(InteractionTimeout());
+			this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+		}
+
+		IEnumerator InteractionTimeout()
+		{
+			yield return new WaitForSeconds(7);
+		}
+
+		public void Populate(NPCObject info)
+		{
+			this.npcInfo.type = info.type;
+			this.npcInfo.varName = info.varName;
+			this.npcInfo.value = info.value;
+			this.gameObject.GetComponent<TextMesh>().text = npcInfo.ToString();
+		}
+	}
+
+}
