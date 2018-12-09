8c1e8a5e572f8b584502528a0d8bf14bb8824b97
FFrostYY <faticfilip98@gmail.com>
2018-12-09 16:33:40 +0100

    created interaction script for methods

---
+++ Assets/Scripts/Method.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project
+{
+	public class Method : MonoBehaviour {
+
+			MethodObject npcInfo;
+			public string npcText;
+			// Use this for initialization
+			void Start () {
+				//DEBUG STATEMENTS
+				//NPCObject testInfo = new NPCObject("integer", "ExampleName");
+				//Populate(testInfo);
+			}
+			
+			// Update is called once per frame
+			void Update () {
+				
+			}
+			
+			public void Interact()
+			{
+				Debug.Log("Interaction method run");
+				Debug.Log(this.npcText);
+				gameObject.transform.Find("Text").GetComponent<TextMesh>().text = this.npcText;
+				StartCoroutine(InteractionTimeout());
+			}
+
+			IEnumerator InteractionTimeout()
+			{
+				yield return new WaitForSeconds(7);
+				this.gameObject.transform.Find("Text").GetComponent<TextMesh>().text = "";
+			}
+
+			public void Populate(MethodObject info)
+			{
+				Debug.Log("Populated: " + info.ToString());
+				this.npcText = info.ToString();
+				Debug.Log("DoubleCheck: " + this.npcText);
+			}
+	}
+}
---
+++ Assets/Scripts/Models/MethodObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project
+{
+		[Serializable]
+		public class MethodObject {
+
+		public string type;
+		public string name;
+
+			public MethodObject(string type, string name)
+			{
+				this.name = name;
+				this.type = type;
+			}
+			public override string ToString()
+			{
+				string txt = "Name: " + this.name + "\nType: " + this.type + "\n";
+				return txt;
+			}
+
+
+	}
+}
