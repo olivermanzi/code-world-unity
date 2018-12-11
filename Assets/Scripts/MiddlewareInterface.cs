092fcfc3f0b34085b40236310843750a7e7b2205
Jorelsin <jorelsin@gmail.com>
2018-12-11 10:31:49 +0100

    set up the projects, and added a exe file to gitgetter

--- Assets/Scripts/MiddlewareInterface.cs
+++ Assets/Scripts/MiddlewareInterface.cs
@@ -32,7 +32,7 @@ namespace Project
         public void GetJson (Text name) {
             UnityEngine.Debug.Log("Middleware: GetJson run!");
             // Defining some variables
-            string NodePath = UnityEngine.Application.dataPath + "/Resources/Middleware/Client2.exe";
+            string NodePath = UnityEngine.Application.dataPath + "/Resources/Middleware/ConsoleApplication.exe";
             var User = name.text.ToString();
             var Repository = project.text.ToString();
             
