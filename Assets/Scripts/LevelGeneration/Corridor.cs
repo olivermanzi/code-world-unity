e79bdf4268a3f31b77a2ca7078253404366ef9c5
Filip Fatic <gusfatfi@student.gu.se>
2019-01-11 19:22:00 +0100

    added the class name indicator

--- Assets/Scripts/LevelGeneration/Corridor.cs
+++ Assets/Scripts/LevelGeneration/Corridor.cs
@@ -3,56 +3,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Corridor : MonoBehaviour {
+namespace Project
+{
+        public class Corridor : MonoBehaviour {
 
-    public string IsExtensionOf { get; set; }
-    public string[] ConnectionNames { get; set; }
+        public string IsExtensionOf { get; set; }
+        public string[] ConnectionNames { get; set; }
 
-    public List<GameObject> Connections;
-    private List<GameObject> _doors;
+        public List<GameObject> Connections;
+        private List<GameObject> _doors;
 
-    public void SetupConnections()
-    {
-        Connections = new List<GameObject>();
-        InitializeDoors();
-        foreach (var item in ConnectionNames)
+        public void SetupConnections()
         {
-            Connect(GetNextEmptyDoor(), item);
-        }
-    }
-
-    private void InitializeDoors()
-    {
-        _doors = new List<GameObject>();
-        var portals = transform.GetComponentsInChildren<Portal>();
-        foreach (var item in portals)
-        {
-            if(item.receiver == null)
+            Connections = new List<GameObject>();
+            InitializeDoors();
+            foreach (var item in ConnectionNames)
             {
-                GameObject gate = item.transform.parent.gameObject;
-                _doors.Add(gate);
+                Connect(GetNextEmptyDoor(), item);
             }
         }
-    }
 
-    private GameObject GetNextEmptyDoor()
-    {
-        var door = _doors.Find(x => _doors.IndexOf(x) == _doors.Count - 1);
-        _doors.Remove(door);
-        return door;
-    }
-
-    private void Connect(GameObject door, string name)
-    {
-        var target = GameObject.Find(name);
-        if (target != null)
+        private void InitializeDoors()
         {
-            Transform destination = target.transform;
-            door.GetComponentInChildren<Portal>().receiver = destination.GetComponent<RoomBehaviour>().BackTeleporter.transform;
-            door.GetComponentInChildren<Portal>().destination = name;
-            door.transform.parent.GetComponentInChildren<TextMesh>().text = name;
-            destination.Find("PortalCamera").GetComponent<PortalCamera>().OtherPortal = door.transform.Find("Portal").GetComponent<MeshRenderer>().transform;
-            Connections.Add(target);
+            _doors = new List<GameObject>();
+            var portals = transform.GetComponentsInChildren<Portal>();
+            foreach (var item in portals)
+            {
+                if(item.receiver == null)
+                {
+                    GameObject gate = item.transform.parent.gameObject;
+                    _doors.Add(gate);
+                }
+            }
+        }
+
+        private GameObject GetNextEmptyDoor()
+        {
+            var door = _doors.Find(x => _doors.IndexOf(x) == _doors.Count - 1);
+            _doors.Remove(door);
+            return door;
+        }
+
+        private void Connect(GameObject door, string name)
+        {
+            var target = GameObject.Find(name);
+            if (target != null)
+            {
+                Transform destination = target.transform;
+                door.GetComponentInChildren<Portal>().receiver = destination.GetComponent<RoomBehaviour>().BackTeleporter.transform;
+                door.GetComponentInChildren<Portal>().destination = name;
+                door.transform.parent.GetComponentInChildren<TextMesh>().text = name;
+                destination.Find("PortalCamera").GetComponent<PortalCamera>().OtherPortal = door.transform.Find("Portal").GetComponent<MeshRenderer>().transform;
+                Connections.Add(target);
+            }
         }
     }
 }
--- Assets/Scripts/LevelGeneration/PortalCameraManager.cs
+++ Assets/Scripts/LevelGeneration/PortalCameraManager.cs
@@ -3,74 +3,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PortalCameraManager : MonoBehaviour {
+namespace Project
+{
+    public class PortalCameraManager : MonoBehaviour {
 
-    public GameObject[] environmentObjects;
-    public Transform player;
+        public GameObject[] environmentObjects;
+        public Transform player;
 
-    public void CycleCameras()
-    {
-        Transform playerLocation = null;
-        foreach (var item in environmentObjects)
+        public void CycleCameras()
         {
-            //Deactivate Cameras
-            item.transform.Find("PortalCamera").gameObject.SetActive(false);
-            //Find which room the player is in
-            if (player.transform.position.y - item.transform.position.y < 15f && player.transform.position.y - item.transform.position.y > 10f)
+            Transform playerLocation = FindPlayer();
+            if(playerLocation != null)
             {
-                playerLocation = item.transform;
+                EnableCorrectCameras(playerLocation);
             }
         }
-        if(playerLocation != null)
-        {
-            EnableCorrectCameras(playerLocation);
-        }
-    }
 
-    private void EnableCorrectCameras(Transform playerLocation)
-    {
-        GameObject[] directConnections = GetConnections(playerLocation);
-        foreach (var directCon in directConnections)
+        public Transform FindPlayer()
         {
-            SetObserverCamera(directCon, player.transform.GetChild(0));
-            GameObject[] indirectConnections = GetConnections(directCon.transform);
-            foreach (var indirectCon in indirectConnections)
+            Transform playerLocation = null;
+            foreach (var item in environmentObjects)
             {
-                SetObserverCamera(indirectCon, directCon.transform.Find("PortalCamera"));
-            }
-        }
-    }
-
-    private GameObject[] GetConnections(Transform location)
-    {
-        GameObject[] connections = null;
-        if (location.GetComponent<RoomBehaviour>() != null)
-        {
-            var loc = location.GetComponent<RoomBehaviour>();
-            connections = loc.subrooms.ToArray();
-        }
-        else
-        {
-            var loc = location.GetComponent<Corridor>();
-            connections = loc.Connections.ToArray();
-        }
-        return connections;
-    }
-
-    private void SetObserverCamera(GameObject location, Transform newCamera)
-    {
-        var camera = location.transform.Find("PortalCamera").GetComponent<PortalCamera>();
-        camera.playerCamera = newCamera;
-        foreach (var item in camera.otherPortals)
-        {
-            foreach (var mr in location.transform.GetComponentsInChildren<MeshRenderer>())
-            {
-                if (item.Equals(mr))
+                //Deactivate Cameras
+                item.transform.Find("PortalCamera").gameObject.SetActive(false);
+                //Find which room the player is in
+                if (player.transform.position.y - item.transform.position.y < 15f && player.transform.position.y - item.transform.position.y > 10f)
                 {
-                    camera.OtherPortal = mr.transform;
+                    playerLocation = item.transform;
+                    Debug.Log("1: " + playerLocation.gameObject.name);
+                    return playerLocation;
+                }
+            }
+            return playerLocation;
+        }
+
+        private void EnableCorrectCameras(Transform playerLocation)
+        {
+            GameObject[] directConnections = GetConnections(playerLocation);
+            foreach (var directCon in directConnections)
+            {
+                SetObserverCamera(directCon, player.transform.GetChild(0));
+                GameObject[] indirectConnections = GetConnections(directCon.transform);
+                foreach (var indirectCon in indirectConnections)
+                {
+                    SetObserverCamera(indirectCon, directCon.transform.Find("PortalCamera"));
                 }
             }
         }
-        camera.gameObject.SetActive(true);
+
+        private GameObject[] GetConnections(Transform location)
+        {
+            GameObject[] connections = null;
+            if (location.GetComponent<RoomBehaviour>() != null)
+            {
+                var loc = location.GetComponent<RoomBehaviour>();
+                connections = loc.subrooms.ToArray();
+            }
+            else
+            {
+                var loc = location.GetComponent<Corridor>();
+                connections = loc.Connections.ToArray();
+            }
+            return connections;
+        }
+
+        private void SetObserverCamera(GameObject location, Transform newCamera)
+        {
+            var camera = location.transform.Find("PortalCamera").GetComponent<PortalCamera>();
+            camera.playerCamera = newCamera;
+            foreach (var item in camera.otherPortals)
+            {
+                foreach (var mr in location.transform.GetComponentsInChildren<MeshRenderer>())
+                {
+                    if (item.Equals(mr))
+                    {
+                        camera.OtherPortal = mr.transform;
+                    }
+                }
+            }
+            camera.gameObject.SetActive(true);
+        }
     }
 }
