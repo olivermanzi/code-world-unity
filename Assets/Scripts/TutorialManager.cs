84d965be1505717a950e0d2e177443f0982c5f76
Bence Ludmány <mokkants@gmail.com>
2019-01-14 07:53:36 +0100

    Enable cycling through tutorials

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,16 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class TutorialManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    GameObject[] tutCanvases;
+
+    GameObject fpCanvas;
+
+    // Find canvases
+    void Start () {
+        var tutCanvas1 = GameObject.Find("TutorialCanvas");
+        var tutCanvas2 = GameObject.Find("TutorialCanvas2");
+        tutCanvases = new GameObject[] { tutCanvas1, tutCanvas2 };
+
+        fpCanvas = GameObject.Find("FirstPersonCanvas");
+        fpCanvas.SetActive(false); //Disable fps canvas so cursor doesnt poke through
+    }
+
+    // Update is called once per frame
+    void Update () {
+        EnterListener();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private int canvasCounter = 0;
+
+    private void EnterListener()
+    {
+        //On enter remove next in sequence tutorial canvas
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Destroy(tutCanvases[canvasCounter]);
+            canvasCounter++;
+            //When tutorial is done remove this GameObject from the scene and enable player UI
+            if (canvasCounter >= tutCanvases.Length)
+            {
+                fpCanvas.SetActive(true);
+                Destroy(gameObject);
+            }
+        }
+    }
 }
